051cf9464f40ee1b885882e1615bcd03cb983ba5
home digital <benjaminassah328@gmail.com>
2026-02-04 01:29:58 +0000

    suppression d'une Hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import Header from "@/components/header"
 import Hero from "@/components/hero"
-import Services from "@/components/services"
 import Products from "@/components/products"
 import Footer from "@/components/footer"
 export default function Homepage(){
